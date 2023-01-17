225a1d63c625754cdaa1c98341af3a7d2ace6fe0
Zachary Rose <zacerose@ut.utm.edu>
2023-01-17 13:57:29 -0600

    Basic guessing game complete. User guesses a number between 0 and 100, with hints on whether the guess is too high or low.

--- Guessing Game CSCI352/Program.cs
+++ Guessing Game CSCI352/Program.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Guessing_Game_CSCI352
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+            int guess_the_number = rand.Next(101);
+            Console.WriteLine("Guess the number: ");
+            // arbitrary number outside of possible random generated number
+            int num_guess = -5;
+            string input;
+            int guess_count = 0;
+            while (num_guess != guess_the_number)
+            {
+               input = Console.ReadLine();
+               if (int.TryParse(input, out num_guess))
+               {
+                    guess_count++;
+                    // correct guess
+                    if (num_guess == guess_the_number)
+                    {
+                        Console.WriteLine("Correct! That took " + guess_count + " tries.");
+                    }
+                    // guess was too low
+                    else if (num_guess < guess_the_number)
+                    {
+                        Console.WriteLine("Too low. Try again: ");
+                    }
+                    // too high
+                    else
+                    {
+                        Console.WriteLine("Too high. Try again: ");
+                    }
+               }
+            }
+            
             Console.ReadKey();
         }
     }
